711861ad6b60b324c39d1ebb5dbe8306b98aaa02
Tushar Sharma <tushar29sharma4848@gmail.com>
2025-12-23 14:01:28 +0530

    Remove trailing slash from BASE_URL constant

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
   //export const BASE_URL = "/api";
-  export const BASE_URL = "https://devx-bqtr.onrender.com/";
+  export const BASE_URL = "https://devx-bqtr.onrender.com";
 //export const BASE_URL = "http://localhost:7777"
