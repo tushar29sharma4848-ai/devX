317e8831512b78e4edc98d1ed617341e542aa917
Tushar Sharma <tushar29sharma4848@gmail.com>
2025-12-23 14:06:12 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ const http = require("http");
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://devx-bqtr.onrender.com/",
+    origin: "https://devx-bqtr.onrender.com",
     credentials:true,
 }));
 
